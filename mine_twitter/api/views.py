a5337c1a1966d9d1192d6c4ce11e1ed18d0e4933
Arthur Holanda <holandaarthur46@gmail.com>
2024-10-26 19:51:33 -0300

    View Registrar usuário

--- mine_twitter/api/views.py
+++ mine_twitter/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .serializers.usuario_serializer import UsuarioSerializer
+from api.models import UsuarioCustomizado
 
-# Create your views here.
+class UsuarioCreateView(generics.CreateAPIView):
+    queryset = UsuarioCustomizado.objects.all()
+    serializer_class = UsuarioSerializer
+    permission_classes = [permissions.AllowAny]  # Permitir que qualquer um possa se registrar
+
