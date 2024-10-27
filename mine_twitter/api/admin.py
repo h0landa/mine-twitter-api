6c794d4e7653e9e97cadcf849257156f17d06278
Arthur Holanda <holandaarthur46@gmail.com>
2024-10-27 18:54:18 -0300

    Adição de curtida ao admin

--- mine_twitter/api/admin.py
+++ mine_twitter/api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from api.models import Dispositivo, UsuarioCustomizado, Postagem
+from api.models import Dispositivo, UsuarioCustomizado, Postagem, Curtida
 
 
 admin.site.register(Dispositivo)
 admin.site.register(UsuarioCustomizado)
 admin.site.register(Postagem)
+admin.site.register(Curtida)
