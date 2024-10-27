9ce0e4c4bff9965d8d4040f1761e48ac1af2999d
Arthur Holanda <holandaarthur46@gmail.com>
2024-10-27 18:56:17 -0300

    View Curtir Postagem

--- mine_twitter/api/views/views_postagem.py
+++ mine_twitter/api/views/views_postagem.py
@@ -2,8 +2,8 @@ from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
-from api.serializers import PostagemSerializer
-from api.models import Postagem
+from api.serializers import PostagemSerializer, CurtidaSerializer
+from api.models import Postagem, Curtida, UsuarioCustomizado
 
 
 class CriarPostagemView(generics.CreateAPIView):
@@ -54,3 +54,30 @@ class DeletarPostagemView(generics.DestroyAPIView):
 
     def perform_update(self, serializer):
         serializer.delete()
+
+
+class CurtirPostagemView(generics.CreateAPIView):
+    serializer_class = CurtidaSerializer
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        usuario = request.user
+        postagem_id = request.data.get('postagem')
+
+        if not postagem_id:
+            return Response({'detail': 'Postagem ID é necessário.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            postagem = Postagem.objects.get(id=postagem_id)
+        except Postagem.DoesNotExist:
+            return Response({'detail': 'Postagem não encontrada.'}, status=status.HTTP_404_NOT_FOUND)
+
+        curtida, created = Curtida.objects.get_or_create(usuario=usuario, postagem=postagem)
+
+        if created:
+            return Response({'detail': 'Curtida adicionada.', 'data': CurtidaSerializer(curtida).data}, status=status.HTTP_201_CREATED)
+        else:
+            curtida.delete()
+            return Response({'detail': 'Curtida removida.'}, status=status.HTTP_204_NO_CONTENT)
+
+        
