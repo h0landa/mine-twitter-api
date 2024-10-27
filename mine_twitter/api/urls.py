6dbf63415eec17a027c5aebd87ecce514bf619ba
Arthur Holanda <holandaarthur46@gmail.com>
2024-10-26 21:40:19 -0300

    Nova url de delete usuario

--- mine_twitter/api/urls.py
+++ mine_twitter/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import UsuarioCreateView
+from .views import RegistrarUsuarioView, DeletarUsuarioView
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('registrar_usuario/', UsuarioCreateView.as_view(), name='registrar_usuario'),
+    path('registrar_usuario/', RegistrarUsuarioView.as_view(), name='registrar_usuario'),
+    path('deletar_usuario/<int:id>', DeletarUsuarioView.as_view(), name='deletar_usuario'),
 ]
