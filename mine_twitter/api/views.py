213dfa42c06d963c130b18a99e7fe87c60168e06
Arthur Holanda <holandaarthur46@gmail.com>
2024-10-26 21:40:52 -0300

    Renomeando view

--- mine_twitter/api/views.py
+++ mine_twitter/api/views.py
@@ -1,9 +1,19 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
 from .serializers.usuario_serializer import UsuarioSerializer
+from rest_framework.permissions import IsAuthenticated
 from api.models import UsuarioCustomizado
 
-class UsuarioCreateView(generics.CreateAPIView):
+class RegistrarUsuarioView(generics.CreateAPIView):
     queryset = UsuarioCustomizado.objects.all()
     serializer_class = UsuarioSerializer
-    permission_classes = [permissions.AllowAny]  # Permitir que qualquer um possa se registrar
+    permission_classes = [permissions.AllowAny]
+
+
+class DeletarUsuarioView(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UsuarioSerializer
+    queryset = UsuarioCustomizado.objects.all()
+    lookup_field = 'id'
+
 
