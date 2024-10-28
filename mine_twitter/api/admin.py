003da1111b2009df8859abefc5bd782d58b19fec
Arthur Holanda <holandaarthur46@gmail.com>
2024-10-27 22:26:23 -0300

    Registrando CurtidaPostagem no Admin

--- mine_twitter/api/admin.py
+++ mine_twitter/api/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from api.models import Dispositivo, UsuarioCustomizado, Postagem, Curtida
-
+from api.models import Dispositivo, UsuarioCustomizado, Postagem, CurtidaPostagem
 
 admin.site.register(Dispositivo)
 admin.site.register(UsuarioCustomizado)
 admin.site.register(Postagem)
-admin.site.register(Curtida)
+admin.site.register(CurtidaPostagem)
