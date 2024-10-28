cfcb143ab39c86aef2ec6610845cc469f129b944
Arthur Holanda <holandaarthur46@gmail.com>
2024-10-27 22:31:22 -0300

    Views Usuario

--- mine_twitter/api/views/views_usuario.py
+++ mine_twitter/api/views/views_usuario.py
@@ -2,8 +2,8 @@ from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from api.serializers import UsuarioSerializer
 from rest_framework.permissions import IsAuthenticated
-from api.models import UsuarioCustomizado
-
+from api.models import UsuarioCustomizado, Seguidores
+from api.serializers import SeguidoresSerializer
 
 class RegistrarUsuarioView(generics.CreateAPIView):
     queryset = UsuarioCustomizado.objects.all()
@@ -17,6 +17,16 @@ class EditarUsuarioView(generics.UpdateAPIView):
 
     def get_object(self):
         return self.request.user
+    
+
+class VerPerfilUsuarioView(generics.RetrieveAPIView):
+    serializer_class = UsuarioSerializer
+    permission_classes = [permissions.AllowAny]
+    lookup_url_kwarg = 'usuario_id'
+
+    def get_queryset(self):
+        usuario_id = self.kwargs.get(self.lookup_url_kwarg)
+        return UsuarioCustomizado.objects.filter(id=usuario_id)
     
 
 class AlterarSenhaView(generics.UpdateAPIView):
@@ -35,4 +45,25 @@ class AlterarSenhaView(generics.UpdateAPIView):
         usuario.save()
 
         return Response({"detail": "Senha alterada com sucesso."}, status=status.HTTP_200_OK)
-    
+
+
+class SeguirUsuarioView(generics.CreateAPIView):
+    serializer_class = SeguidoresSerializer
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        usuario_seguidor = request.user
+        usuario_seguido_id = request.data.get('usuario_seguido')
+        
+        if not usuario_seguido_id:
+            return Response({"error": "O campo 'usuario_seguido' é obrigatório."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if Seguidores.objects.filter(usuario_seguidor=usuario_seguidor, usuario_seguido_id=usuario_seguido_id).exists():
+            return Response({"detail": "Você já segue esse usuário."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(usuario_seguidor=usuario_seguidor)
+        
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
