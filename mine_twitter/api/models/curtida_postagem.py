dee4a62e8534051cb02d2b4fb08baabc6a3f36d3
Arthur Holanda <holandaarthur46@gmail.com>
2024-10-27 22:28:25 -0300

    Remodulando Curtida

--- mine_twitter/api/models/curtida_postagem.py
+++ mine_twitter/api/models/curtida_postagem.py
@@ -3,7 +3,7 @@ from .usuario_customizado import UsuarioCustomizado
 from .postagem import Postagem
 
 
-class Curtida(models.Model):
+class CurtidaPostagem(models.Model):
     usuario = models.ForeignKey(UsuarioCustomizado, on_delete=models.CASCADE)
     postagem = models.ForeignKey(Postagem, on_delete=models.CASCADE)
     data_curtida = models.DateTimeField(auto_now_add=True)
